51aa7f6cb5c9a2defbe1df60d6e8b44b735a47a1
Yuri <syt8945@163.com>
2025-05-27 19:11:22 +0800

    增加串口重定向

--- Drivers/Devices/dev_uart/dev_uart.c
+++ Drivers/Devices/dev_uart/dev_uart.c
@@ -60,11 +60,16 @@ void com_usart_init(void)
     usart_enable(USART0);
 }
 /* retarget the C library printf function to the USART */
-// int fputc(int ch, FILE *f)
-// {
-//     usart_data_transmit(USART0, (uint8_t) ch);
 
-//     while(RESET == usart_flag_get(USART0, USART_FLAG_TBE));
-
-//     return ch;
-// }
+int fputc(int ch, FILE *f)
+{
+    usart_data_transmit(USART0, (uint8_t) ch);
+    while(RESET == usart_flag_get(USART0, USART_FLAG_TBE));
+    return ch;
+}
+int  __io_putchar(int ch) 
+{
+    usart_data_transmit(USART0, (uint8_t) ch);
+    while(RESET == usart_flag_get(USART0, USART_FLAG_TBE));
+    return ch;
+}
--- Drivers/Devices/dev_uart/dev_uart.h
+++ Drivers/Devices/dev_uart/dev_uart.h
@@ -4,4 +4,5 @@
 #include "gd32h7xx.h"
 void com_usart_init(void);
 int fputc(int ch, FILE *f);
+int __io_putchar(int ch) ;
 #endif // __DEV_UART_H__
