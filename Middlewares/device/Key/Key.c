2ae8706235023a20b516c0a644e65dc73ea62b06
Yuri <syt8945@163.com>
2025-06-09 12:42:58 +0800

    增加电源开关控制和模式切换功能

--- Middlewares/device/Key/Key.c
+++ Middlewares/device/Key/Key.c
@@ -35,8 +35,9 @@ static void __KeyCycHandle(void);
 static void __KeyInitHandle(void *msg);
 static void __KeyRegister(TypdefKeyBSPCfg *cfg);
 static void __KeyScan(void);
-static void key_short_press_callback(TypdefKeyStatus *key_status);
-static void key_short_press_callback(TypdefKeyStatus *key_status);
+static void __KeyShortPressCallback(TypdefKeyStatus *key_status);
+static void __KeyLongPressCallback(TypdefKeyStatus *key_status);
+static void __KeyToggleCallback(TypdefKeyStatus *key_status);
 
 // const TypdefKeyBSPCfg KeyBspCfg[KeyChannelMax] = {
 
@@ -86,19 +87,16 @@ SYSTEM_REGISTER_INIT(BoardInitStage, KeyPriority, KeyDeviceInit, KeyDeviceInit);
 #endif
 static void __KeyCreateTaskHandle(void) {
     for (size_t i = 0; i < KeyChannelMax; i++) {
-        TypdefKeyStatus *KeyStatusHandle      = &KeyStatus[i];
-        KeyStatusHandle->cfg                  = NULL;
-        KeyStatusHandle->enable               = 0;
-        KeyStatusHandle->state                = 0;
-        KeyStatusHandle->press_time           = 0;
-        KeyStatusHandle->long_press_triggered = 0;
+        memset(&KeyStatus[i], 0, sizeof(TypdefKeyStatus));
+        TypdefKeyStatus *KeyStatusHandle = &KeyStatus[i];
+        KeyStatusHandle->last_pin_value  = 0xFF;  // Initialize last pin value to 0xFF
     }
     xTaskCreate(VFBTaskFrame, "VFBTaskKey", configMINIMAL_STACK_SIZE * 2, (void *)&Key_task_cfg, KeyPriority, NULL);
 }
 SYSTEM_REGISTER_INIT(ServerInitStage, KeyPriority, __KeyCreateTaskHandle, __KeyCreateTaskHandle init);
 
 static void __KeyInitHandle(void *msg) {
-    elog_i(TAG, "__KeyInitHandle\r\n");
+    elog_i(TAG, "__KeyInitHandle");
     elog_set_filter_tag_lvl(TAG, KeyLogLvl);
     vfb_send(KeyStart, 0, NULL, 0);
 }
@@ -115,7 +113,6 @@ static void __KeyRcvHandle(void *msg) {
 
         case KeyRegister: {
             __KeyRegister((TypdefKeyBSPCfg *)MSG_GET_PAYLOAD(tmp_msg));
-            elog_i(TAG, "KeyRegister: %s", ((TypdefKeyBSPCfg *)MSG_GET_PAYLOAD(tmp_msg))->device_name);
         } break;
 
         default:
@@ -137,7 +134,7 @@ static void __KeyCycHandle(void) {
     }
     __KeyScan();
 }
-static void key_short_press_callback(TypdefKeyStatus *key_status) {
+static void __KeyShortPressCallback(TypdefKeyStatus *key_status) {
     if (key_status->cfg->short_press_event == 0) {
         // elog_e(TAG, "Short press event is not configured for key %s\r\n", key_status->cfg->device_name);
         return;
@@ -145,13 +142,24 @@ static void key_short_press_callback(TypdefKeyStatus *key_status) {
     elog_i(TAG, "Key %s short pressed\r\n", key_status->cfg->device_name);
     vfb_send(key_status->cfg->short_press_event, 0, NULL, 0);
 }
-static void key_long_press_callback(TypdefKeyStatus *key_status) {
+static void __KeyLongPressCallback(TypdefKeyStatus *key_status) {
     if (key_status->cfg->long_press_event == 0) {
         // elog_e(TAG, "Long press event is not configured for key %s\r\n", key_status->cfg->device_name);
         return;
     }
     elog_i(TAG, "Key %s long pressed\r\n", key_status->cfg->device_name);
     vfb_send(key_status->cfg->long_press_event, 0, NULL, 0);
+}
+static void __KeyToggleCallback(TypdefKeyStatus *key_status) {
+    if (key_status == NULL || key_status->cfg == NULL) {
+        elog_e(TAG, "Key status or configuration is NULL\r\n");
+        return;
+    }
+    if (key_status->cfg->toggle_event == 0) {
+        return;
+    }
+    elog_i(TAG, "Key %s toggled, state: %d\r\n", key_status->cfg->device_name, key_status->state);
+    vfb_send(key_status->cfg->toggle_event, key_status->state, NULL, 0);
 }
 // 注册按键,遍历KeyStatus查找cfg为空,并新增
 static void __KeyRegister(TypdefKeyBSPCfg *cfg) {
@@ -171,7 +179,7 @@ static void __KeyRegister(TypdefKeyBSPCfg *cfg) {
         if (KeyStatus[i].cfg == NULL) {
             KeyStatus[i].cfg = pvPortMalloc(sizeof(TypdefKeyBSPCfg));
             if (KeyStatus[i].cfg == NULL) {
-                elog_e(TAG, "Failed to allocate memory for key configuration.\r\n");
+                elog_e(TAG, "Failed to allocate memory for key configuration.");
                 return;
             }
             memcpy(KeyStatus[i].cfg, cfg, sizeof(TypdefKeyBSPCfg));
@@ -179,10 +187,11 @@ static void __KeyRegister(TypdefKeyBSPCfg *cfg) {
             KeyStatus[i].state                = 0;
             KeyStatus[i].press_time           = 0;
             KeyStatus[i].long_press_triggered = 0;
+            KeyStatus[i].last_pin_value       = 0xFF;  // Initialize last pin value to 0xFF
 
             /* InitGPIO */
             DevPinInit(&cfg->pin);
-            elog_i(TAG, "Key %s registered successfully on slot %d\r\n", cfg->device_name, i);
+            elog_i(TAG, "Key %s registered successfully on slot %d", cfg->device_name, i);
             return;
         }
     }
@@ -195,27 +204,63 @@ static void __KeyScan(void) {
             continue;  // Skip if key is not registered or disabled
         }
         uint32_t current_time = xTaskGetTickCount() * portTICK_PERIOD_MS;
-        uint8_t key_value     = DevPinRead(&key_states->cfg->pin);
+        uint8_t pin_value     = DevPinRead(&key_states->cfg->pin);
+        uint8_t active_level  = key_states->cfg->active_level;
+        uint8_t triger_mode   = key_states->cfg->trigger_mode;
+        uint8_t key_value     = KEY_INACTIVE;
+        // Adjust key value based on active level
 
-        if (key_value == 0 && key_states->state == 0) {
-            // Key pressed
-            key_states->state                = 1;
-            key_states->press_time           = current_time;
-            key_states->long_press_triggered = 0;
-        } else if (key_value == 0 && key_states->state == 1) {
-            // Key is still pressed
-            uint32_t press_duration = current_time - key_states->press_time;
-            if (press_duration >= LONG_PRESS_THRESHOLD && !key_states->long_press_triggered) {
-                key_long_press_callback(key_states);
-                key_states->long_press_triggered = 1;
-            }
-        } else if (key_value == 1 && key_states->state == 1) {
-            // Key released
-            uint32_t press_duration = current_time - key_states->press_time;
-            if (press_duration < LONG_PRESS_THRESHOLD) {
-                key_short_press_callback(key_states);
-            }
-            key_states->state = 0;
+        switch (triger_mode) {
+            case KEY_TRIGGER_MODE_PRESS:  // 按键模式
+            {
+                key_value = (pin_value == active_level) ? KEY_ACTIVE : KEY_INACTIVE;  // Active level is pressed
+                if (key_value == KEY_ACTIVE && key_states->state == 0) {
+                    // Key pressed
+                    key_states->state                = 1;
+                    key_states->press_time           = current_time;
+                    key_states->long_press_triggered = 0;
+                } else if (key_value == KEY_ACTIVE && key_states->state == 1) {
+                    // Key is still pressed
+                    uint32_t press_duration = current_time - key_states->press_time;
+                    if (press_duration >= LONG_PRESS_THRESHOLD && !key_states->long_press_triggered) {
+                        __KeyLongPressCallback(key_states);
+                        key_states->long_press_triggered = 1;
+                    }
+                } else if (key_value == KEY_INACTIVE && key_states->state == 1) {
+                    // Key released
+                    uint32_t press_duration = current_time - key_states->press_time;
+                    if (press_duration < LONG_PRESS_THRESHOLD && press_duration >= SHORT_PRESS_THRESHOLD) {
+                        __KeyShortPressCallback(key_states);
+                    }
+                    key_states->state = 0;
+                }
+            } break;
+            case KEY_TRIGGER_MODE_TOGGLE: {                                           // 自锁模式
+                key_value = (pin_value == active_level) ? KEY_ACTIVE : KEY_INACTIVE;  // Active level is pressed
+                if (key_value != key_states->last_pin_value) {
+                    // Key state changed
+                    key_states->last_pin_value       = key_value;  // Update last pin value
+                    key_states->press_time           = current_time;
+                    key_states->long_press_triggered = 0;
+                    elog_i(TAG, "Key %s state changed: %d", key_states->cfg->device_name, key_value);
+                } else if (key_value == key_states->last_pin_value && key_states->long_press_triggered == 0) {
+                    uint32_t press_duration = current_time - key_states->press_time;
+                    if (press_duration >= SHORT_PRESS_THRESHOLD) {
+                        key_states->state = key_value;
+                        __KeyToggleCallback(key_states);
+                        key_states->long_press_triggered = 1;
+                    } else {
+                        // Key state unchanged, do nothing
+                        // elog_i(TAG, "Key %s state unchanged: %d", key_states->cfg->device_name, key_value);
+                    }
+                } else {
+                    // 按键未变化,或者按键处于消抖等待状态,不做处理
+                    // elog_i(TAG, "Key %s state unchanged: %d", key_states->cfg->device_name, key_value);
+                }
+            } break;
+            default:
+                elog_e(TAG, "Unknown trigger mode: %d\r\n", triger_mode);
+                continue;  // Skip this key if the trigger mode is unknown
         }
     }
 }
--- Middlewares/device/Key/Key.h
+++ Middlewares/device/Key/Key.h
@@ -1,16 +1,30 @@
 #ifndef __KEY_H__
 #define __KEY_H__
 #include <stdio.h>
+
 #include "dev_basic.h"
 #include "dev_pin.h"
+typedef enum {
+    KEY_TRIGGER_MODE_NONE = 0,  // 无触发模式
+    KEY_TRIGGER_MODE_PRESS,   // 按键模式
+    KEY_TRIGGER_MODE_TOGGLE,  // 自锁模式
 
+} EnumKeyTirgerMode;
+typedef enum {
+    KEY_INACTIVE,
+    KEY_ACTIVE,  
+
+
+} EnumKeyActiveState;
 typedef struct
 {
     char device_name[DEVICE_NAME_MAX];
-    DevPinHandleStruct pin;      // GPIO 配置
-    uint8_t active_level;        // Active level, 0 for low, 1 for high
-    uint32_t short_press_event;  // Short press event
-    uint32_t long_press_event;   // Long press event
+    DevPinHandleStruct pin;          // GPIO 配置
+    EnumKeyTirgerMode trigger_mode;  // 按键触发模式
+    uint8_t active_level;            // Active level, 0 for low, 1 for high
+    uint32_t short_press_event;      // Short press event
+    uint32_t long_press_event;       // Long press event
+    uint32_t toggle_event;           // Toggle event
 
 } TypdefKeyBSPCfg;
 
@@ -18,8 +32,9 @@ typedef struct
 {
     uint8_t enable;  // Enable flag
     TypdefKeyBSPCfg *cfg;
-    uint8_t state;
+    uint8_t state;//用于按键模式
+    uint8_t last_pin_value;  // Last pin value ,用于自锁模式,位置为0xFF表示初始化
     uint32_t press_time;
     uint8_t long_press_triggered;
 } TypdefKeyStatus;
-#endif // __KEY_H__
+#endif  // __KEY_H__
