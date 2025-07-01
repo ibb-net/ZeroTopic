d3cce4950ce4059cf66751c46e724953dabbd1a9
Yuri <syt8945@163.com>
2025-07-01 19:45:43 +0800

    优化uart启动时序

--- Middlewares/device/Uart/Uart.c
+++ Middlewares/device/Uart/Uart.c
@@ -28,8 +28,7 @@
 #define CONFIG_Uart_CYCLE_TIMER_MS 100
 #endif
 /* ===================================================================================== */
-typedef struct
-{
+typedef struct {
     /* TX GPIO*/
     DevPinHandleStruct tx_gpio_cfg;
     DevPinHandleStruct rx_gpio_cfg;
@@ -50,45 +49,47 @@ void UartStreamRcvTask(void *arg);
 const TypdefUartBSPCfg UartBspCfg[UartChannelMax] = {
     {
 
-        .tx_gpio_cfg = {
-            .device_name = "Uart_TX",
-            .base        = Uart_TX_GPIO_PORT,
-            .af          = Uart_TX_GPIO_AF,
-            .pin         = Uart_TX_GPIO_PIN,
-        },
-        .rx_gpio_cfg = {
-            .device_name = "Uart_RX",
-            .base        = Uart_RX_GPIO_PORT,
-            .af          = Uart_RX_GPIO_AF,
-            .pin         = Uart_RX_GPIO_PIN,
-        },
-        .uart_cfg = {
-            .id           = 0,  // ID
-            .device_name  = "Uart_UART",
-            .base         = Uart_UART_BASE,
-            .baudrate     = Uart_UART_BAUDRATE,
-            .idle_timeout = Uart_UART_IDLE_TIMEOUT,
-            .rx_isr_cb    = __UartRXISRHandle,  // RX ISR callback function
-            .tx_isr_cb    = NULL,               // TX ISR callback function
-            .error_isr_cb = NULL,               // Error ISR callback function
-            /* DMA */
-            .tx_dma_rcu       = Uart_TX_DMA_BASE_ADDR,
-            .tx_dma_base_addr = Uart_TX_DMA_BASE_ADDR,
-            .tx_dma_channel   = Uart_TX_DMA_CHANNEL,
-            .tx_dma_request   = Uart_TX_DMA_REQUEST,
-            .tx_dma_isr_cb    = __UartTXDMAISRHandle,  // TX DMA ISR callback function
-            .rx_dma_rcu       = Uart_RX_DMA_BASE_ADDR,
-            .rx_dma_base_addr = Uart_RX_DMA_BASE_ADDR,
-            .rx_dma_channel   = Uart_RX_DMA_CHANNEL,
-            .rx_dma_request   = Uart_RX_DMA_REQUEST,
-            .rx_dma_isr_cb    = __UartRXISRHandle,  // RX DMA ISR callback function
-        },
+        .tx_gpio_cfg =
+            {
+                .device_name = "Uart_TX",
+                .base        = Uart_TX_GPIO_PORT,
+                .af          = Uart_TX_GPIO_AF,
+                .pin         = Uart_TX_GPIO_PIN,
+            },
+        .rx_gpio_cfg =
+            {
+                .device_name = "Uart_RX",
+                .base        = Uart_RX_GPIO_PORT,
+                .af          = Uart_RX_GPIO_AF,
+                .pin         = Uart_RX_GPIO_PIN,
+            },
+        .uart_cfg =
+            {
+                .id           = 0,  // ID
+                .device_name  = "Uart_UART",
+                .base         = Uart_UART_BASE,
+                .baudrate     = Uart_UART_BAUDRATE,
+                .idle_timeout = Uart_UART_IDLE_TIMEOUT,
+                .rx_isr_cb    = __UartRXISRHandle,  // RX ISR callback function
+                .tx_isr_cb    = NULL,               // TX ISR callback function
+                .error_isr_cb = NULL,               // Error ISR callback function
+                /* DMA */
+                .tx_dma_rcu       = Uart_TX_DMA_BASE_ADDR,
+                .tx_dma_base_addr = Uart_TX_DMA_BASE_ADDR,
+                .tx_dma_channel   = Uart_TX_DMA_CHANNEL,
+                .tx_dma_request   = Uart_TX_DMA_REQUEST,
+                .tx_dma_isr_cb    = __UartTXDMAISRHandle,  // TX DMA ISR callback function
+                .rx_dma_rcu       = Uart_RX_DMA_BASE_ADDR,
+                .rx_dma_base_addr = Uart_RX_DMA_BASE_ADDR,
+                .rx_dma_channel   = Uart_RX_DMA_CHANNEL,
+                .rx_dma_request   = Uart_RX_DMA_REQUEST,
+                .rx_dma_isr_cb    = __UartRXISRHandle,  // RX DMA ISR callback function
+            },
         .buffer_size = Uart_UART_BUFFER_SIZE,
     },
 };
 
-typedef struct
-{
+typedef struct {
     uint32_t uart_base;
     char device_name[DEVICE_NAME_MAX];
     uint32_t id;  // ID
@@ -115,28 +116,24 @@ TypdefUartStatus UartStatus[UartChannelMax] = {0};
 /* ===================================================================================== */
 
 static const vfb_event_t UartEventList[] = {
-    UartStart,
-    UartStop,
-    UartSet,
-    UartGet,
-    UartSend,
-    UartRcv,
+    UartStart, UartStop, UartSet, UartGet, UartSend, UartRcv,
 
 };
 
 static const VFBTaskStruct Uart_task_cfg = {
     .name         = "VFBTaskUart",  // Task name
     .pvParameters = NULL,
-    // .uxPriority = 10,											  // Task parameters
-    .queue_num               = 32,                                           // Number of queues to subscribe
-    .event_list              = UartEventList,                                // Event list to subscribe
-    .event_num               = sizeof(UartEventList) / sizeof(vfb_event_t),  // Number of events to subscribe
-    .startup_wait_event_list = NULL,                                         // Events to wait for at startup
-    .startup_wait_event_num  = 0,                                            // Number of startup events to wait for
-    .xTicksToWait            = pdMS_TO_TICKS(CONFIG_Uart_CYCLE_TIMER_MS),    // Wait indefinitely
-    .init_msg_cb             = UartInitHandle,                               // Callback for initialization messages
-    .rcv_msg_cb              = UartRcvHandle,                                // Callback for received messages
-    .rcv_timeout_cb          = UartCycHandle,                                // Callback for timeout
+    // .uxPriority = 10,
+    // // Task parameters
+    .queue_num  = 32,                                           // Number of queues to subscribe
+    .event_list = UartEventList,                                // Event list to subscribe
+    .event_num  = sizeof(UartEventList) / sizeof(vfb_event_t),  // Number of events to subscribe
+    .startup_wait_event_list = NULL,                            // Events to wait for at startup
+    .startup_wait_event_num  = 0,  // Number of startup events to wait for
+    .xTicksToWait            = pdMS_TO_TICKS(CONFIG_Uart_CYCLE_TIMER_MS),  // Wait indefinitely
+    .init_msg_cb             = UartInitHandle,  // Callback for initialization messages
+    .rcv_msg_cb              = UartRcvHandle,   // Callback for received messages
+    .rcv_timeout_cb          = UartCycHandle,   // Callback for timeout
 };
 
 /* ===================================================================================== */
@@ -179,12 +176,14 @@ void UartDeviceInit(void) {
         }
         uart_handle->rx_buffer_for_vfb = pvPortMalloc(UartBspCfg[i].buffer_size);
         if (uart_handle->rx_buffer_for_vfb == NULL) {
-            elog_e(TAG, "[Fault]malloc %s buffer for vfb failed!\r\n", UartBspCfg[i].uart_cfg.device_name);
+            elog_e(TAG, "[Fault]malloc %s buffer for vfb failed!\r\n",
+                   UartBspCfg[i].uart_cfg.device_name);
             vPortFree(uart_handle->rx_buffer);
             while (1);
             return;
         }
-        DevUartDMARecive(&(UartBspCfg[i].uart_cfg), uart_handle->rx_buffer, UartBspCfg[i].buffer_size);
+        DevUartDMARecive(&(UartBspCfg[i].uart_cfg), uart_handle->rx_buffer,
+                         UartBspCfg[i].buffer_size);
 
         memset(uart_handle->device_name, 0, sizeof(uart_handle->device_name));
         snprintf(uart_handle->device_name, sizeof(uart_handle->device_name), "Uart%d", i);
@@ -194,7 +193,6 @@ void UartDeviceInit(void) {
             elog_e(TAG, "create %s mutex failed!\r\n", uart_handle->device_name);
             return;
         }
-        DevUartStart(&(UartBspCfg[i].uart_cfg));
     }
 }
 // SYSTEM_REGISTER_INIT(MCUInitStage, UartPriority, UartDeviceInit, UartDeviceInit);
@@ -202,10 +200,13 @@ void UartDeviceInit(void) {
 static void UartCreateTaskHandle(void) {
     UartDeviceInit();
 
-    xTaskCreate(VFBTaskFrame, "VFBTaskUart", configMINIMAL_STACK_SIZE*2, (void *)&Uart_task_cfg, UartPriority, NULL);
-    xTaskCreate(UartStreamRcvTask, "UartRx", configMINIMAL_STACK_SIZE, (void *)&Uart_task_cfg, UartPriority - 1, NULL);
+    xTaskCreate(VFBTaskFrame, "VFBTaskUart", configMINIMAL_STACK_SIZE * 2, (void *)&Uart_task_cfg,
+                UartPriority, NULL);
+    xTaskCreate(UartStreamRcvTask, "UartRx", configMINIMAL_STACK_SIZE, (void *)&Uart_task_cfg,
+                UartPriority - 1, NULL);
 }
-SYSTEM_REGISTER_INIT(ServerInitStage, UartPriority, UartCreateTaskHandle, UartCreateTaskHandle init);
+SYSTEM_REGISTER_INIT(ServerInitStage, UartPriority, UartCreateTaskHandle,
+                     UartCreateTaskHandle init);
 
 static void UartInitHandle(void *msg) {
     elog_i(TAG, "UartInitHandle\r\n");
@@ -213,11 +214,9 @@ static void UartInitHandle(void *msg) {
     vfb_send(UartStart, 0, NULL, 0);
 }
 // 5A A5 05 82 00A0 007D
-uint8_t buzzer_1s[] = {
-    0x5A, 0xA5, 0x05, 0x82, 0x00, 0xA0, 0x00, 0x7D};
+uint8_t buzzer_1s[] = {0x5A, 0xA5, 0x05, 0x82, 0x00, 0xA0, 0x00, 0x7D};
 
-uint8_t bl_100[] = {
-    0x5A, 0xA5, 0x04, 0x82, 0x00, 0x82, 0x20};
+uint8_t bl_100[] = {0x5A, 0xA5, 0x04, 0x82, 0x00, 0x82, 0x20};
 // 接收消息的回调函数
 static void UartRcvHandle(void *msg) {
     TaskHandle_t curTaskHandle    = xTaskGetCurrentTaskHandle();
@@ -227,8 +226,8 @@ static void UartRcvHandle(void *msg) {
     switch (tmp_msg->frame->head.event) {
         case UartStart: {
             elog_i(TAG, "UartStart %d", tmp_msg->frame->head.data);
-            // vfb_send(UartSend, 0, buzzer_1s, sizeof(buzzer_1s));  // 测试蜂鸣器
-           
+            DevUartStart(&(UartBspCfg[0].uart_cfg));
+
         } break;
         case UartStop: {
         } break;
@@ -243,7 +242,8 @@ static void UartRcvHandle(void *msg) {
             }
             elog_d(TAG, "UartSend: length: %d", MSG_GET_LENGTH(tmp_msg));
             // elog_hexdump(TAG, 16, MSG_GET_PAYLOAD(tmp_msg), MSG_GET_LENGTH(tmp_msg));
-            DevUartDMASend(&UartBspCfg[0].uart_cfg, (const uint8_t *)MSG_GET_PAYLOAD(tmp_msg), MSG_GET_LENGTH(tmp_msg));
+            DevUartDMASend(&UartBspCfg[0].uart_cfg, (const uint8_t *)MSG_GET_PAYLOAD(tmp_msg),
+                           MSG_GET_LENGTH(tmp_msg));
             // wait for TX DMA complete lock_tx
             if (uart_handle->lock_tx != NULL) {
                 if (xSemaphoreTake(uart_handle->lock_tx, pdMS_TO_TICKS(300)) == pdFALSE) {
@@ -291,11 +291,14 @@ void UartStreamRcvTask(void *arg) {
     int rcv_count                 = 0;
     memset(uart_handle->rx_buffer_for_vfb, 0, uart_handle->buffer_size);
     while (1) {
-        rcv_count = xStreamBufferReceive(uart_handle->rx_stream_buffer, uart_handle->rx_buffer_for_vfb, uart_handle->buffer_size, portMAX_DELAY);
+        rcv_count =
+            xStreamBufferReceive(uart_handle->rx_stream_buffer, uart_handle->rx_buffer_for_vfb,
+                                 uart_handle->buffer_size, portMAX_DELAY);
         if (rcv_count > 0) {
             vfb_send(UartRcv, 0, uart_handle->rx_buffer_for_vfb, rcv_count);
             memset(uart_handle->rx_buffer_for_vfb, 0, rcv_count);
         }
+        
     }
 }
 static void __UartRXISRHandle(void *arg) {
@@ -310,7 +313,8 @@ static void __UartRXISRHandle(void *arg) {
         printf("\r\n[ERROR]DMA transfer number is zero, no data received.\r\n");
     }
     int rx_count = uart_handle->buffer_size - dma_transfer_number;
-    // printf("RX ISR: rx_count = %d, buffer_size = %d ,dma_transfer_number = %d\r\n", rx_count, uart_handle->buffer_size, dma_transfer_number);
+    // printf("RX ISR: rx_count = %d, buffer_size = %d ,dma_transfer_number = %d\r\n", rx_count,
+    // uart_handle->buffer_size, dma_transfer_number);
     if (rx_count <= 0) {
         printf("\r\n[ERROR]RX count is zero or negative, no data received.\r\n");
     } else {
@@ -318,10 +322,12 @@ static void __UartRXISRHandle(void *arg) {
 const void *pvItemToQueue,
 BaseType_t *pxHigherPriorityTaskWoken ); */
         BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-        if (xStreamBufferSendFromISR(uart_handle->rx_stream_buffer, uart_handle->rx_buffer, rx_count, &xHigherPriorityTaskWoken) == 0) {
+        if (xStreamBufferSendFromISR(uart_handle->rx_stream_buffer, uart_handle->rx_buffer,
+                                     rx_count, &xHigherPriorityTaskWoken) == 0) {
             printf("\r\n[ERROR]Failed to send data to stream buffer from ISR\r\n");
         }
-        DevUartDMARecive(&(uart_handle->UartBspCfg->uart_cfg), uart_handle->rx_buffer, uart_handle->buffer_size);
+        DevUartDMARecive(&(uart_handle->UartBspCfg->uart_cfg), uart_handle->rx_buffer,
+                         uart_handle->buffer_size);
     }
 }
 static void __UartTXDMAISRHandle(void *arg) {
