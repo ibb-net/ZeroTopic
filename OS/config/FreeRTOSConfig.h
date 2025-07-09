941f7fa25a3e49e1e9c93a7453e13ad502651e7d
Yuri <syt8945@163.com>
2025-07-09 10:42:04 +0800

    关闭堆栈溢出检测

--- OS/config/FreeRTOSConfig.h
+++ OS/config/FreeRTOSConfig.h
@@ -82,7 +82,8 @@
 #define configIDLE_SHOULD_YIELD			1
 #define configUSE_MUTEXES				1
 #define configQUEUE_REGISTRY_SIZE		8
-#define configCHECK_FOR_STACK_OVERFLOW	2
+/* 释放后关闭 开启为2 */
+#define configCHECK_FOR_STACK_OVERFLOW	0
 #define configUSE_RECURSIVE_MUTEXES		1
 #define configUSE_MALLOC_FAILED_HOOK	0
 #define configUSE_APPLICATION_TASK_TAG	0
